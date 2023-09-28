62f7353bb5e0bd8d38295cba4fca4a225203891c
Allan Murillo González <allanjs88@Allans-MacBook-Pro.local>
2023-09-28 11:33:06 -0600

    Jpeg compresion

--- jpeg.py
+++ jpeg.py
@@ -1,43 +1,201 @@
-import numpy as np
+# python 3.9.5
+
+# Author: mVirtuoso21
+# Git: https://github.com/mVirtuoso21/JPEG-Image-Compressor/tree/main
+from math import ceil
+
 import cv2
+import numpy as np
 
-# Load the image using OpenCV
-image = cv2.imread("Carro.jpg")
+from functions import *
 
-#1. Color Space Conversion: Convert the image to YCbCr color space
-ycbcr_image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+# define quantization tables
+QTY = np.array([[16, 11, 10, 16, 24, 40, 51, 61],  # luminance quantization table
+                [12, 12, 14, 19, 26, 48, 60, 55],
+                [14, 13, 16, 24, 40, 57, 69, 56],
+                [14, 17, 22, 29, 51, 87, 80, 62],
+                [18, 22, 37, 56, 68, 109, 103, 77],
+                [24, 35, 55, 64, 81, 104, 113, 92],
+                [49, 64, 78, 87, 103, 121, 120, 101],
+                [72, 92, 95, 98, 112, 100, 103, 99]])
+
+QTC = np.array([[17, 18, 24, 47, 99, 99, 99, 99],  # chrominance quantization table
+                [18, 21, 26, 66, 99, 99, 99, 99],
+                [24, 26, 56, 99, 99, 99, 99, 99],
+                [47, 66, 99, 99, 99, 99, 99, 99],
+                [99, 99, 99, 99, 99, 99, 99, 99],
+                [99, 99, 99, 99, 99, 99, 99, 99],
+                [99, 99, 99, 99, 99, 99, 99, 99],
+                [99, 99, 99, 99, 99, 99, 99, 99]])
+# define window size
+windowSize = len(QTY)
+
+# read image
+imgOriginal = cv2.imread('marbles.bmp', cv2.IMREAD_COLOR)
+
+###########################
+#1. Conversión del espacio de color
+###########################
+
+# Convert BGR to YCrCb
+img = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2YCR_CB)
+width = len(img[0])
+height = len(img)
+y = np.zeros((height, width), np.float32) + img[:, :, 0]
+cr = np.zeros((height, width), np.float32) + img[:, :, 1]
+cb = np.zeros((height, width), np.float32) + img[:, :, 2]
+
+###########################
+#2. Reducción de resolución de crominancia:
+###########################
+
+# size of the image in bits before compression
+totalNumberOfBitsWithoutCompression = len(y) * len(y[0]) * 8 + len(cb) * len(cb[0]) * 8 + len(cr) * len(cr[0]) * 8
+# channel values should be normalized, hence subtract 128
+y = y - 128
+cr = cr - 128
+cb = cb - 128
 
 
-#2. Chroma Subsampling
+# 4: 2: 2 subsampling is used # another subsampling scheme can be used
+# thus chrominance channels should be sub-sampled
+# define subsampling factors in both horizontal and vertical directions
+SSH, SSV = 2, 2
 
-# Apply chroma subsampling (e.g., 4:2:0 subsampling)
-#subsampling_ratio = (2, 2)  # (horizontal subsampling factor, vertical subsampling factor)
+###########################
+#División en bloques de pixeles de 8x8
+###########################
 
-# Perform subsampling on the Cb and Cr channels
-#ycbcr_image[:, :, 1::] = cv2.resize(ycbcr_image[:, :, 1::], None, fx=1/subsampling_ratio[0], fy=1/subsampling_ratio[1], interpolation=cv2.INTER_LINEAR)
+# filter the chrominance channels using a 2x2 averaging filter # another type of filter can be used
+crf = cv2.boxFilter(cr, ddepth=-1, ksize=(2, 2))
+cbf = cv2.boxFilter(cb, ddepth=-1, ksize=(2, 2))
+crSub = crf[::SSV, ::SSH]
+cbSub = cbf[::SSV, ::SSH]
 
-#3. Block Splitting: Split the image into 8x8 blocks.
+###########################
+# 4. Transformada de coseno discreta DCT
+###########################
 
-#4. Discrete Cosine Transform (DCT): Perform the discrete cosine transform (DCT) on each channel
-dct_image = np.zeros_like(ycbcr_image, dtype=np.float32)
-for i in range(3):
-    dct_image[:, :, i] = cv2.dct(np.float32(ycbcr_image[:, :, i]))
+# check if padding is needed,
+# if yes define empty arrays to pad each channel DCT with zeros if necessary
+yWidth, yLength = ceil(len(y[0]) / windowSize) * windowSize, ceil(len(y) / windowSize) * windowSize
+if (len(y[0]) % windowSize == 0) and (len(y) % windowSize == 0):
+    yPadded = y.copy()
+else:
+    yPadded = np.zeros((yLength, yWidth))
+    for i in range(len(y)):
+        for j in range(len(y[0])):
+            yPadded[i, j] += y[i, j]
 
-# 5. Quantize the DCT coefficients (you can define your quantization matrix)
-quantization_matrix = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
-                                [12, 12, 14, 19, 26, 58, 60, 55],
-                                [14, 13, 16, 24, 40, 57, 69, 56],
-                                [14, 17, 22, 29, 51, 87, 80, 62],
-                                [18, 22, 37, 56, 68, 109, 103, 77],
-                                [24, 35, 55, 64, 81, 104, 113, 92],
-                                [49, 64, 78, 87, 103, 121, 120, 101],
-                                [72, 92, 95, 98, 112, 100, 103, 99]])
+# chrominance channels have the same dimensions, meaning both can be padded in one loop
+cWidth, cLength = ceil(len(cbSub[0]) / windowSize) * windowSize, ceil(len(cbSub) / windowSize) * windowSize
+if (len(cbSub[0]) % windowSize == 0) and (len(cbSub) % windowSize == 0):
+    crPadded = crSub.copy()
+    cbPadded = cbSub.copy()
+# since chrominance channels have the same dimensions, one loop is enough
+else:
+    crPadded = np.zeros((cLength, cWidth))
+    cbPadded = np.zeros((cLength, cWidth))
+    for i in range(len(crSub)):
+        for j in range(len(crSub[0])):
+            crPadded[i, j] += crSub[i, j]
+            cbPadded[i, j] += cbSub[i, j]
 
-print(dct_image.shape)
+# get DCT of each channel
+# define three empty matrices
+yDct, crDct, cbDct = np.zeros((yLength, yWidth)), np.zeros((cLength, cWidth)), np.zeros((cLength, cWidth))
 
-quantized_dct_image = np.zeros_like(dct_image)
-for i in range(3):
-    quantized_dct_image[:, :, i] = np.round(dct_image[:, :, i] / quantization_matrix)
+# number of iteration on x axis and y axis to calculate the luminance cosine transform values
+hBlocksForY = int(len(yDct[0]) / windowSize)  # number of blocks in the horizontal direction for luminance
+vBlocksForY = int(len(yDct) / windowSize)  # number of blocks in the vertical direction for luminance
+# number of iteration on x axis and y axis to calculate the chrominance channels cosine transforms values
+hBlocksForC = int(len(crDct[0]) / windowSize)  # number of blocks in the horizontal direction for chrominance
+vBlocksForC = int(len(crDct) / windowSize)  # number of blocks in the vertical direction for chrominance
 
-# Save the quantized DCT coefficients as a compressed image
-cv2.imwrite("compressed_image.jpg", quantized_dct_image.astype(np.uint8))
+# define 3 empty matrices to store the quantized values
+yq, crq, cbq = np.zeros((yLength, yWidth)), np.zeros((cLength, cWidth)), np.zeros((cLength, cWidth))
+# and another 3 for the zigzags
+yZigzag = np.zeros(((vBlocksForY * hBlocksForY), windowSize * windowSize))
+crZigzag = np.zeros(((vBlocksForC * hBlocksForC), windowSize * windowSize))
+cbZigzag = np.zeros(((vBlocksForC * hBlocksForC), windowSize * windowSize))
+
+
+###########################
+# 4. Cuantización
+###########################
+
+yCounter = 0
+for i in range(vBlocksForY):
+    for j in range(hBlocksForY):
+        yDct[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] = cv2.dct(
+            yPadded[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize])
+        yq[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] = np.ceil(
+            yDct[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] / QTY)
+        yZigzag[yCounter] += zigzag(
+            yq[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize])
+        yCounter += 1
+yZigzag = yZigzag.astype(np.int16)
+
+# either crq or cbq can be used to compute the number of blocks
+cCounter = 0
+for i in range(vBlocksForC):
+    for j in range(hBlocksForC):
+        crDct[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] = cv2.dct(
+            crPadded[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize])
+        crq[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] = np.ceil(
+            crDct[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] / QTC)
+        crZigzag[cCounter] += zigzag(
+            crq[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize])
+        cbDct[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] = cv2.dct(
+            cbPadded[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize])
+        cbq[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] = np.ceil(
+            cbDct[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize] / QTC)
+        cbZigzag[cCounter] += zigzag(
+            cbq[i * windowSize: i * windowSize + windowSize, j * windowSize: j * windowSize + windowSize])
+        cCounter += 1
+crZigzag = crZigzag.astype(np.int16)
+cbZigzag = cbZigzag.astype(np.int16)
+
+###########################
+# 5. Run length and Huffman Encondign
+###########################
+
+# find the run length encoding for each channel
+# then get the frequency of each component in order to form a Huffman dictionary
+yEncoded = run_length_encoding(yZigzag)
+yFrequencyTable = get_freq_dict(yEncoded)
+yHuffman = find_huffman(yFrequencyTable)
+
+crEncoded = run_length_encoding(crZigzag)
+crFrequencyTable = get_freq_dict(crEncoded)
+crHuffman = find_huffman(crFrequencyTable)
+
+cbEncoded = run_length_encoding(cbZigzag)
+cbFrequencyTable = get_freq_dict(cbEncoded)
+cbHuffman = find_huffman(cbFrequencyTable)
+
+# calculate the number of bits to transmit for each channel
+# and write them to an output file
+file = open("CompressedImage.asfh", "w")
+yBitsToTransmit = str()
+for value in yEncoded:
+    yBitsToTransmit += yHuffman[value]
+
+crBitsToTransmit = str()
+for value in crEncoded:
+    crBitsToTransmit += crHuffman[value]
+
+cbBitsToTransmit = str()
+for value in cbEncoded:
+    cbBitsToTransmit += cbHuffman[value]
+
+if file.writable():
+    file.write(yBitsToTransmit + "\n" + crBitsToTransmit + "\n" + cbBitsToTransmit)
+
+
+file.close()
+
+totalNumberOfBitsAfterCompression = len(yBitsToTransmit) + len(crBitsToTransmit) + len(cbBitsToTransmit)
+print(
+    "Compression Ratio is " + str(
+        np.round(totalNumberOfBitsWithoutCompression / totalNumberOfBitsAfterCompression, 1)))
