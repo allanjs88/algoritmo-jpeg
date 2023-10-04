9e8ebcd1c3036e66d557f09e268d1998b9c6b6b1
Brian Morera <brianmorera@microsoft.com>
2023-10-03 20:59:52 -0600

    Encrypting images with jpeg and webp

--- main.py
+++ main.py
@@ -4,23 +4,31 @@ from jpeg.jpeg_pillow import encode_and_save_image_jpeg, decode_and_save_image_j
 from webp.webp_pillow import encode_and_save_image_webp, decode_and_save_image_webp
 
 IMAGE_PATH = "images/"
-ENCRYPTED_IMAGE_PATH = "encrypted_images/"
+ENCRYPTED_JPEG_IMAGE_PATH = "jpeg_images/"
+ENCRYPTED_WEBP_IMAGE_PATH = "webp_images/"
 
-if __name__ == '__main__':
-
-    create_dir(ENCRYPTED_IMAGE_PATH)
-    images = os.listdir(IMAGE_PATH)
+def generate_input_images(is_jpeg, images_path, dest_path):
+    create_dir(dest_path)
+    images = os.listdir(images_path)
 
     for image in images:
         print("Imagen: "+ image)
         if is_image(image):
             extension = get_file_extension(image)
-            input_image_path = IMAGE_PATH + image
+            input_image_path = images_path + image
 
-            encrypted_images_path = ENCRYPTED_IMAGE_PATH + image
+            encrypted_images_path = dest_path + image
             encrypted_images_path = encrypted_images_path.replace(extension, '_2.jpg')
 
-            encoded_path = encode_and_save_image_jpeg(input_image_path)
-            decode_and_save_image_jpeg(encoded_path, encrypted_images_path)
+            if is_jpeg:
+                encoded_path = encode_and_save_image_jpeg(input_image_path)
+                decode_and_save_image_jpeg(encoded_path, encrypted_images_path)
+            else:
+                encoded_path = encode_and_save_image_webp(input_image_path)
+                decode_and_save_image_webp(encoded_path, encrypted_images_path)
         else:
             print(f"{image} is not a valid image.")
+
+if __name__ == '__main__':
+    generate_input_images(True, IMAGE_PATH, ENCRYPTED_JPEG_IMAGE_PATH)
+    generate_input_images(False, IMAGE_PATH, ENCRYPTED_WEBP_IMAGE_PATH)
